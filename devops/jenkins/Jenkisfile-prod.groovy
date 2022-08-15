321086ec921ebf4f4e6e9163f190e6afe18a5d78
Fernando <46256429+GanemoCorp@users.noreply.github.com>
2022-08-14 19:53:43 -0500

    Update Jenkisfile-prod.groovy

--- devops/jenkins/Jenkisfile-prod.groovy
+++ devops/jenkins/Jenkisfile-prod.groovy
@@ -1,46 +1,6 @@
+@Library('jenkins-sharedlib')
 
-pipeline { 
-    environment {
-        registry = "odoopartners/odoo-sh:1.0.0"
-        registryCredential = 'dockerhubtestid'
-        dockerImage = ''
+def repo_sh = "https://github.com/MollerSpa/moller.git"
+def submodule_branch = '14.0'
 
-        repo_sh = "https://github.com/MollerSpa/moller.git"
-        submodule_branch = '14.0'
-    }
-    agent any 
-    stages { 
-
-        stage('Preparation') {
-            steps {
-                script {
-                    echo 'Preparing project ...'
-                }
-            }
-        }
-        stage('Pull repos') {
-            steps { 
-                script {
-                    withCredentials([usernamePassword(credentialsId: 'odoopartnersid', passwordVariable: 'TOKEN_VALUE', usernameVariable: 'NOT_USED')]) {
-                        docker.withRegistry( '', registryCredential ) {
-                            sh "docker run --rm -e GIT_TOKEN=${TOKEN_VALUE} -v ${WORKSPACE}/devops/src/repos.csv:/usr/src/app/repos.csv ${registry} python ci_odoo_sh.py -u ${repo_sh} -s ${submodule_branch}"
-                        }
-                    }
-                }
-            } 
-        }
-        stage('Checking state') {
-            steps {
-                script {
-                    echo 'checking state project ...'
-                }
-            }
-        }
-    }
-
-    post {
-        always {
-            deleteDir()
-        }
-    }
-}
+buildPipelineOdooSH(repo_sh: "${repo_sh}", submodule_branch: "${submodule_branch}")
