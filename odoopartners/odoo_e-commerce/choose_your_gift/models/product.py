4846a9ed88d63c27658214a4b85dd91eeb713682
ADMIN-GANEMO <fernando@ganemo.co>
2022-08-26 17:20:45 +0000

    [ADD] SUBMODULES - 26/08/2022 17:20:30  - odoo_e-commerce  - odoo_themes  - CybroAddons

--- odoopartners/odoo_e-commerce/choose_your_gift/models/product.py
+++ odoopartners/odoo_e-commerce/choose_your_gift/models/product.py
@@ -17,9 +17,9 @@ class ProductProduct(models.Model):
 
     def filter_gifts(self, warehouse_id):
         data = []
-        available_threshold = self.product_tmpl_id.available_threshold
         for gift_id in self.gifts_ids:
             virtual_available = gift_id.with_context(warehouse=warehouse_id).virtual_available
+            available_threshold = gift_id.product_tmpl_id.available_threshold
             data.append({
                 'gift_id': gift_id,
                 'error': False if virtual_available > available_threshold else True
