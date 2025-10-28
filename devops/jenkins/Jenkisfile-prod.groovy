b141dbb8d1b5fe4e3fe9e6b02282b48753f883d3
Daniel Blanco <daniel@blancomartin.cl>
2025-10-27 21:05:31 -0300

    [RM] devops

--- devops/jenkins/Jenkisfile-prod.groovy
+++
@@ -1,6 +0,0 @@
-@Library('jenkins-sharedlib')
-
-def repo_sh = "https://github.com/MollerSpa/moller.git"
-def submodule_branch = '14.0'
-
-buildPipelineOdooSH(repo_sh: "${repo_sh}", submodule_branch: "${submodule_branch}")
